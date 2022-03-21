25fb9d6dad116177c4d343489120a3288b582367
usama.arif <usama.arif@LQA-USAMAARIF>
2022-03-21 16:32:36 +0500

    Add project files.

---
+++ SamplePOCProject/Tests/LoginTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+namespace SamplePOCProject
+{
+    [TestFixture]
+    public class LoginTest
+    {
+
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup() {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+        }
+        [Test]
+        public void LoginTestIntoApp()
+        {
+
+        }
+        [TearDown]
+        public void TearDown() {
+            driver.Quit();
+        }
+    }
+}
